9f08d7327577ad8598fa892fc03343cc13d90cd7
Andrew Atimapre <atimaxandrew@gmail.com>
2022-08-24 19:09:00 +0100

    Fixed some bugs

--- src/components/SendGetPaid.jsx
+++ src/components/SendGetPaid.jsx
@@ -7,24 +7,24 @@ const SendGetPaid = () => {
     <section id="sendgetpaid-section">
       <div className="flex flex-col mx-4 mb-7 font-dmsans md:mx-32 md:flex-row">
         <div className="flex flex-col mb-7 bg-gray-50 rounded-2xl md:mb-14 md:mr-3 md:w-1/2">
-          <div className="w-48 mx-auto pt-24">
+          <div className="w-28 mx-auto pt-16 md:pt-24 md:w-48">
             <img src={sendImg} alt="Send cash" />
           </div>
-          <div className="mx-auto px-8 pt-16 md:px-16">
+          <div className="mx-auto pt-10 px-8 md:pt-16 md:px-16">
             <h1 className="text-xl font-bold md:text-3xl">
               Send cash to family & friends
             </h1>
-            <p className="text-lg pt-5 pb-16 md:text-xl">
+            <p className="text-lg pt-2 pb-16 md:pt-5 md:text-xl">
               Who remembers long account numbers anymore? Just use the Payday
               $tag
             </p>
           </div>
         </div>
         <div className="flex flex-col bg-veryDarkBlue rounded-2xl md:ml-3 md:w-1/2">
-          <div className="w-48 mx-auto pt-24">
+          <div className="w-28 pt-16 mx-auto md:pt-24 md:w-48">
             <img src={paidImg} alt="Get paid" />
           </div>
-          <div className="text-white px-8 mx-auto pt-16 md:px-16">
+          <div className="text-white pt-10 px-8 mx-auto md:pt-16 md:px-16">
             <h1 className="text-xl font-bold md:text-4xl">
               Get paid by foreign employers
             </h1>
