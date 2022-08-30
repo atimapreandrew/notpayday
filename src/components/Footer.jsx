d33a20789b92ef686df020d86b40b96fcb7eada9
Andrew Atimapre <atimaxandrew@gmail.com>
2022-08-30 15:26:27 +0100

    Added footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,16 +15,16 @@ function Footer() {
         <div className="flex flex-col mx-10 py-16 md:pb-16 md:pt-0 md:mx-auto">
           <div className="flex flex-row place-items-center pb-5 space-x-3 md:pb-10">
             <a href="">
-              <img className="w-28" src={logo} alt="" />
+              <img className="w-24 md:w-28" src={logo} alt="" />
             </a>
             <p className="text-white">|</p>
             <a href="">
-              <img className="w-28" src={flutterwave} alt="" />
+              <img className="w-24 md:w-28" src={flutterwave} alt="" />
             </a>
           </div>
-          <p className="text-white text-lg">NotPayDay © {year}</p>
+          <p className="text-white text-sm md:text-lg">NotPayDay © {year}</p>
         </div>
-        <div className="space-y-4 text-lg">
+        <div className="space-y-4 text-sm md:text-lg">
           <div className="flex flex-col text-white space-y-4 pb-5 md:pb-16">
             <h1 className="text-gray-300 font-bold">OUR PRODUCTS</h1>
             <a href="">Payday</a>
@@ -43,7 +43,7 @@ function Footer() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col space-y-4 text-white text-lg pb-16">
+        <div className="flex flex-col space-y-4 text-sm text-white pb-16 md:text-lg">
           <h1 className="text-gray-300 font-bold">COMPANY</h1>
           <a href="">About Us</a>
           <a href="">Ambassadors</a>
@@ -53,7 +53,7 @@ function Footer() {
           <a href="">Blog</a>
           <a href="">FAQ</a>
         </div>
-        <div className="text-white space-y-4 text-lg pb-16">
+        <div className="text-white space-y-4 text-sm pb-16 md:text-lg">
           <h1 className="text-gray-300 font-bold">LOCATIONS</h1>
           <p className="flex place-items-center">
             <img className="w-5 h-5 rounded-full mr-1" src={ca} alt="" />: 2800
